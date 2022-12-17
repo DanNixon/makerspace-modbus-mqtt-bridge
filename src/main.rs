1332685147b02f80422f616d08ddf43625ac0a38
Dan Nixon <dan@dan-nixon.com>
2022-12-17 13:27:05 +0000

    Log at INFO level by default

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ struct Cli {
 
 #[tokio::main]
 async fn main() {
-    env_logger::init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let cli = Cli::parse();
     log::debug!("{:?}", cli);
